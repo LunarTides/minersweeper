809001391866b7c1ffc9413c6945eb8be9150c40
LunarTides <31688109+LunarTides@users.noreply.github.com>
2023-09-27 09:21:50 +0200

    feat: add base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+struct Board {
+    board: Vec<Vec<char>>,
+    length: u8,
+    height: u8
+}
+
+impl Board {
+    fn new(length: u8, height: u8) -> Board {
+        let board = vec![vec!['-'; length.into()]; height.into()];
+        Board { board, length, height }
+    }
+
+    fn print(&self) {
+        for i in &self.board {
+            for v in i {
+                print!("{} ", v);
+            }
+            println!();
+        }
+    }
+}
+
+struct Console {}
+
+impl Console {
+    fn new() -> Console {
+        Console {}
+    }
+
+    fn clear(&self) {
+        print!("\x1bc");
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let console = Console::new();
+    console.clear();
+
+    let board = Board::new(5, 5);
+    board.print();
 }
